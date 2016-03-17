64822d6cd542643310b7fde6a2a4852822d2906f
S. Bratieres <sb358@cam.ac.uk>
2016-03-17 17:54:27 +0000

    tests

--- src/gpstruct_map.py
+++ src/gpstruct_map.py
@@ -28,7 +28,7 @@ def sample_given_f(data_train, f):
     return Y
 
 def expected_statistics(data_train, f):
-    """ computes the expected count of average statistics, ie the term d log Z/df. this is one of the two terms of 
+    """ computes the expected count of average statistics, ie the term d log Z/df. this is one of the two terms of
     d log lik (D_train) / df"""
     n_samples = 50 # computing expectation over fixed number of samples -- surely there are cleverer schemes
     accumulated_statistics = np.zeros_like(f, dtype=util.dtype_for_arrays)
@@ -41,7 +41,7 @@ def grad_likelihood(data_train, f):
 
 def compute_Kinv_f(f, L):
     return L.solve_cholesky_lower(f) # K^-1 f = (L L^T)^-1 f
-    
+
 def grad_prior(f, L):
     return -L.solve_cholesky_lower(f)
 
@@ -77,10 +77,11 @@ def obtain_logger(config):
     #ch.setLevel(logging.INFO) # avoid clogging console (more info in file)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
-    
-    fh = logging.FileHandler(config['result_prefix'] + "log")
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+
+    if 'result_prefix' in config:
+        fh = logging.FileHandler(config['result_prefix'] + "log")
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
 
     return logger
 
@@ -113,32 +114,43 @@ def run(config):
 
     os.makedirs(config['result_prefix'], exist_ok=True)
     logger=obtain_logger(config)
-    
+
     import prepare_from_data_chain
-    (ll_train, 
-    posterior_marginals_test, 
-    compute_error_nlm, 
-    ll_test, 
-    average_marginals, 
+    (ll_train,
+    posterior_marginals_test,
+    compute_error_nlm,
+    ll_test,
+    average_marginals,
     write_marginals,
     read_marginals,
-    n_labels, 
-    X_train, 
-    X_test) = prepare_from_data_chain.prepare_from_data_chain(task=config['task'], 
+    n_labels,
+    X_train,
+    X_test) = prepare_from_data_chain.prepare_from_data_chain(task=config['task'],
                                                            logger=logger,
-                                                           **common_arguments                                                          
+                                                           **common_arguments
                                                )
 
     # prepare K, L=cholK, and then func and grad for the minimizer
-    kernel = kernels.kernel_exponential_ard
-    lhp = {'unary': np.log(1), 'binary': np.log(1), 'jitter': np.log(1e-4),
-                           'variances' : +1 * np.ones((n_features))} 
+    #kernel = kernels.kernel_exponential_ard
+    #lhp = {'unary': np.log(1), 'binary': np.log(1), 'jitter': np.log(1e-4), 'variances' : +1 * np.ones((n_features))}
+    kernel = kernels.kernel_linear
+    lhp = {'unary': np.log(1), 'binary': np.log(1), 'jitter': np.log(1e-4)}
     L = kernels.compute_lower_chol_k(kernel, lhp, X_train, n_labels)
     func_L = lambda x : func(x,L, ll_train, logger)
     grad_L = lambda x : grad(x,L, data_train, logger)
-    u = np.random.randn(data_train.n_labels * data_train.X.shape[0] + data_train.n_labels **2)
-    x0 = L.dot(u.astype(util.dtype_for_arrays))
-    res = scipy.optimize.minimize(func_L, x0, method=config['optimization.method'], 
+
+    # how to choose x0 ?
+    if True:
+        # read off stored experiment results
+        with open('/xvdb/results-2015-06-23/pygpstruct_50k.task=%(task)s.hp_sampling_mode=None.parameterization=ub.fold=%(fold)g.hp_sampling_thinning=1000/state.pickle'
+                  % config, 'rb') as file:
+            x0 = pickle.load(file)['current_f']
+    else:
+        # pick random position
+        u = np.random.randn(data_train.n_labels * data_train.X.shape[0] + data_train.n_labels **2)
+        x0 = L.dot(u.astype(util.dtype_for_arrays))
+
+    res = scipy.optimize.minimize(func_L, x0, method=config['optimization.method'],
                                   jac=grad_L, options=config['optimization.options'])
     f_star_mean = kernels.compute_k_star_T_k_inv(kernel, lhp, X_train, X_test, n_labels, L).dot(res.x)
     marginals_f_star = posterior_marginals_test(f_star_mean)
@@ -152,5 +164,79 @@ def run(config):
         pickle.dump(result, result_file)
     logger.info(result)
 
-    
-# TODO add already written tests here
+
+# TESTS
+if __name__ == "__main__":
+
+    # TEST setup, copied from run() ------------
+    config = {'task': 'segmentation', 'fold':0, 'pygpstruct_location': '/xvdb/pygpstruct/'}
+
+    n_features = {'basenp' : 6438, 'chunking' : 29764, 'segmentation' : 1386, 'japanesene' : 102799}[config['task']]
+    n_data = {'basenp' : 300, 'chunking' : 100, 'segmentation' : 36, 'japanesene' : 100}[config['task']]
+    n_data_train = {'basenp' : 150, 'chunking' : 50, 'segmentation' : 20, 'japanesene' : 50}[config['task']]
+    fold_indices = np.loadtxt(config['pygpstruct_location'] + '/data/datasplit.n_data=%s.txt' % n_data, dtype=np.int16) - 1 # need -1 because doing +1 inside prepare_data_chain
+
+    common_arguments = {
+    'data_indices_train' : fold_indices[config['fold'],:n_data_train],
+    'data_indices_test' : fold_indices[config['fold'],n_data_train:],
+    'data_folder' : config['pygpstruct_location'] + '/data/' + config['task'],
+    'native_implementation': True,
+    }
+
+    import dataset_chain
+    data_train = dataset_chain.dataset_chain(task = config['task'], data_folder=common_arguments['data_folder'], sub_indices=common_arguments['data_indices_train'])
+    data_test = dataset_chain.dataset_chain(task = config['task'], data_folder=common_arguments['data_folder'], sub_indices=common_arguments['data_indices_test'])
+    # will need to use inside prepare_from_data.posterior_marginals
+
+    logger=obtain_logger(config)
+    logger.setLevel(logging.INFO)
+
+    import prepare_from_data_chain
+    (ll_train,
+    posterior_marginals_test,
+    compute_error_nlm,
+    ll_test,
+    average_marginals,
+    write_marginals,
+    read_marginals,
+    n_labels,
+    X_train,
+    X_test) = prepare_from_data_chain.prepare_from_data_chain(task=config['task'],
+                                                           logger=logger,
+                                                           **common_arguments
+                                               )
+
+    # prepare K, L=cholK, and then func and grad for the minimizer
+    #kernel = kernels.kernel_exponential_ard
+    #lhp = {'unary': np.log(1), 'binary': np.log(1), 'jitter': np.log(1e-4), 'variances' : +1 * np.ones((n_features))}
+    kernel = kernels.kernel_linear
+    lhp = {'unary': np.log(1), 'binary': np.log(1), 'jitter': np.log(1e-4)}
+    L = kernels.compute_lower_chol_k(kernel, lhp, X_train, n_labels)
+    func_L = lambda x : func(x,L, ll_train, logger)
+    grad_L = lambda x : grad(x,L, data_train, logger)
+
+    u = np.random.randn(data_train.n_labels * data_train.X.shape[0] + data_train.n_labels **2)
+    x0 = L.dot(u.astype(util.dtype_for_arrays))
+    # END TEST setup, copied from run() ------------
+
+    # TEST
+    # suppose all f's corresponding to binary factors are equal, say 0.
+    # then these two values should be equal:
+    # - #edges in data
+    # - expectation of #all edges over dataset
+    np.testing.assert_almost_equal((data_train.n_points - data_train.N),
+                                   expected_statistics(data_train, f)[-9:].sum())
+
+    # TEST: manual gradient check in one direction
+    # I can do a manual check in one single direction, to have an idea of orders of magnitude: take a step in direction eps * np.ones(), and compare:
+
+    eps = 1e-7
+    np.testing.assert_almost_equal(func_L(x0 + eps) - func_L(x0), # first terms of Taylor expansion
+                                   grad_L(x0).dot(eps*np.ones_like(x0)), # gradient value
+                                   decimal=3)
+    # gradient works ! at least in that direction.
+
+    # TEST: gradient check
+    # check_grad will make a move of amplitude eps into each direction (ie along each dimension of x0), and compute the difference between the finite-diff approx (f(x + eps) - f(x) / eps), and the gradient in that direction
+    assert(scipy.optimize.check_grad(func_L, grad_L, x0, epsilon=0.1) < 1e5)
+    # this is long, because it iterates over the number of dimensions of f-space, eg 3522 in the present configuration
